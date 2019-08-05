1ab131223a5fcf603534c213116660a0336654d2
Flavio Luche <flavioluche@gmail.com>
2019-08-05 06:19:25 -0300

    adds devtools dependency

--- forum/src/main/java/br/com/alura/forum/controller/TopicoController.java
+++ forum/src/main/java/br/com/alura/forum/controller/TopicoController.java
@@ -3,20 +3,20 @@ package br.com.alura.forum.controller;
 import java.util.Arrays;
 import java.util.List;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import br.com.alura.forum.modelo.Curso;
 import br.com.alura.forum.modelo.Topico;
 
-@Controller
+@RestController
+//Com a anotação Restcontroller, a anotação controller não precisa mais ser usada e ela substitui também a anotação ResponseBody para todos os
+// métodos de exposição dos endpoints.
 public class TopicoController {
 
 	@RequestMapping("/topicos")
-	@ResponseBody
 	public List<Topico> lista() {
-		Topico topico = new Topico("Duvida", "Duvida com Spring", new Curso("Spring", "Programação"));
+		Topico topico = new Topico("Dúvida", "Duvida com Spring", new Curso("Spring", "Programação"));
 		
 		return Arrays.asList(topico, topico, topico);
 	}
